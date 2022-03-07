7983ed1196f4b22bb8bd606c73adc27c31fb7331
Iman Taouil <itaouil@c2r8p5.42nice.fr>
2022-03-07 19:46:57 +0100

    norme

---
+++ print_ops.c
@@ -0,0 +1,133 @@
+#include "push_swap.h"
+
+void	print_last_op(t_list *ops)
+{
+	if (ops->content == SA)
+			ft_putstr_fd("sa\n", 1);
+	else if (ops->content == SB)
+		ft_putstr_fd("sb\n", 1);
+	else if (ops->content == PA)
+		ft_putstr_fd("pa\n", 1);
+	else if (ops->content == PB)
+		ft_putstr_fd("pb\n", 1);
+	else if (ops->content == RA)
+		ft_putstr_fd("ra\n", 1);
+	else if (ops->content == RB)
+		ft_putstr_fd("rb\n", 1);
+	else if (ops->content == RRA)
+		ft_putstr_fd("rra\n", 1);
+	else if (ops->content == RRB)
+		ft_putstr_fd("rrb\n", 1);
+}
+
+// static void	check_rr(t_list **ops)
+// {
+// 	if ((((*ops)->content == RA && (*ops)->next->content == RB))
+// 			|| ((*ops)->content == RB && (*ops)->next->content == RA))
+// 	{
+// 		(*ops) = (*ops)->next;
+// 		ft_putstr_fd("rr\n", 1);
+// 	}
+// 	else if ((*ops)->content == RA)
+// 		ft_putstr_fd("ra\n", 1);
+// 	else if ((*ops)->content == RB)
+// 		ft_putstr_fd("rb\n", 1);
+// }
+
+static void	check_ss(t_list **ops)
+{
+	if ((((*ops)->content == SA && (*ops)->next->content == SB))
+			|| ((*ops)->content == SB && (*ops)->next->content == SA))
+	{
+		(*ops) = (*ops)->next;
+		ft_putstr_fd("ss\n", 1);
+	}
+	else if ((*ops)->content == SA)
+		ft_putstr_fd("sa\n", 1);
+	else if ((*ops)->content == SB)
+		ft_putstr_fd("sb\n", 1);
+}
+
+void	print_operations(t_list *ops)
+{
+	while (ops->next)
+	{
+		if (ops->content == SA)
+		// {
+		// 	if (ops->next->content == SB)
+		// 	{
+		// 		ft_putstr_fd("ss\n", 1);
+		// 		ops = ops->next;
+		// 	}
+		// 	else
+		// 		ft_putstr_fd("sa\n", 1);
+			check_ss(&ops);
+		// }
+		else if (ops->content == SB)
+		{
+			// if (ops->next->content == SA)
+			// {
+			// 	ft_putstr_fd("ss\n", 1);
+			// 	ops = ops->next;
+			// }
+			// else
+			// 	ft_putstr_fd("sb\n", 1);
+			check_ss(&ops);
+		}
+		else if (ops->content == PA)
+			ft_putstr_fd("pa\n", 1);
+		else if (ops->content == PB)
+			ft_putstr_fd("pb\n", 1);
+		else if (ops->content == RA)
+		{
+			if (ops->next->content == RRA)
+				ops = ops->next;
+			else if (ops->next->content == RB)
+			{
+				ft_putstr_fd("rr\n", 1);
+				ops = ops->next;
+			}
+			else
+				ft_putstr_fd("ra\n", 1);
+		}
+		else if (ops->content == RB)
+		{
+			if (ops->next->content == RRB)
+				ops = ops->next;
+			else if (ops->next->content == RA)
+			{
+				ft_putstr_fd("rr\n", 1);
+				ops = ops->next;
+			}
+			else
+				ft_putstr_fd("rb\n", 1);
+		}
+		else if (ops->content == RRA)
+		{
+			if (ops->next->content == RA)
+				ops = ops->next;
+			else if (ops->next->content == RRB)
+			{
+				ft_putstr_fd("rrr\n", 1);
+				ops = ops->next;
+			}
+			else
+				ft_putstr_fd("rra\n", 1);
+		}
+		else if (ops->content == RRB)
+		{
+			if (ops->next->content == RB)
+				ops = ops->next;
+			else if (ops->next->content == RRA)
+			{
+				ft_putstr_fd("rrr\n", 1);
+				ops = ops->next;
+			}
+			else
+				ft_putstr_fd("rrb\n", 1);
+		}
+		if (ops->next)
+			ops = ops->next;
+	}
+	print_last_op(ops);
+}
